8cb5dc409f9e4b6e1a140055d5e23f8ed44beb75
twxeq <twxeqq@gmail.com>
2026-08-24 15:45:56 +0200

    1st upload. no @media
    @media here soon

---
+++ mandra/script.js
@@ -0,0 +1,41 @@
+
+function updateDecoAnimation() {
+    const scrollY = window.scrollY;
+    const decocontainer = document.querySelector('.deco-imgs');
+    if (!decocontainer) return;
+
+    const rect = decocontainer.getBoundingClientRect();
+    const scrollprog = window.innerHeight - rect.top;
+
+    const deco1 = document.querySelector('.deco-1');
+    const deco2 = document.querySelector('.deco-2');
+    const deco3 = document.querySelector('.deco-3');
+
+    const offset = scrollY * 0.2;
+
+    let opacityprog = scrollprog / 300;
+
+    opacityprogm = Math.min(Math.max(opacityprog, 0), 1);
+
+    if (deco1 && deco2 && deco3) {
+        deco1.style.transform = `translateX(${200 - offset}px) rotate(34deg)`
+        deco2.style.transform = `translateX(${-170 + offset}px) rotate(-30deg)`
+        deco3.style.transform = `translateX(${-150 + offset}px) rotate(-20deg)`
+
+        deco1.style.opacity = opacityprog;
+        deco2.style.opacity = opacityprog;
+        deco3.style.opacity = opacityprog;
+    }
+
+}
+
+window.addEventListener('DOMContentLoaded', updateDecoAnimation);
+window.addEventListener('scroll', updateDecoAnimation);
+
+
+document.getElementById('trigger').addEventListener('click', () => {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+    });
+});
