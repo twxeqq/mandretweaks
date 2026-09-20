cbd91e30943a9d8b317327a3dde737852fa01113
twxeq <twxeqq@gmail.com>
2026-09-20 22:51:49 +0300

    Почти доделан режим для пк. Начат для телефона
    Закончены: ссылки, карусель, анимка для каталога, бургер для телефона, титл для телефона
    Осталось доделать:
    Пк: баги с картинками
    Телефон: макет расположения, каталог

--- new-mandra/script.js
+++ new-mandra/script.js
@@ -11,3 +11,46 @@ function moveCarousel(direction) {
         behavior: 'smooth'
     });
 }
+
+
+// catalog title
+
+const titleobs = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible');
+        }
+    });
+}, {threshold: 0.3});
+
+const catalogtitle = document.querySelector('.catalog-title');
+if (catalogtitle) {
+    titleobs.observe(catalogtitle);
+}
+
+
+// burger
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    const openbtn = document.querySelector('.openbtn');
+    const closebtn = document.querySelector('.closebtn');
+    const menu = document.querySelector('.menu');
+    if ( openbtn && closebtn ) {
+        openbtn.addEventListener('click', function() {
+            openbtn.classList.add('hidden');
+            closebtn.classList.add('visible');
+            menu.classList.add('visible');
+            document.body.style.overflow = 'hidden';
+        });
+        closebtn.addEventListener('click', function(){
+            openbtn.classList.remove('hidden');
+            closebtn.classList.remove('visible');
+            menu.classList.remove('visible')
+            document.body.style.overflow = ''
+        });
+    }
+
+});
